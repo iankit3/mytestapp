69c310595bdc277deddf2dcf30138f84050f9a24
iankit3 <a4ankit@outlook.com>
2017-05-25 01:54:31 +0530

    F4

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 
 //var path = require('path');
 var app = express();
-app.listen(9090);
+app.set('port', (process.env.PORT || 5000));
  app.get("/api",function(req,res){ 
     connection.executeQuery('SELECT * from users',function(err,rows){
         res.end( JSON.stringify(rows) )
