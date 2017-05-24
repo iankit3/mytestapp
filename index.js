0652b4890928320361a464232e7e073e07093029
iankit3 <a4ankit@outlook.com>
2017-05-25 02:01:08 +0530

    F6

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var express = require('express');
-//var connectionManager = require('./database/connectionManager');
-//var bodyParser = require('body-parser');
-//var connection = require('./database/connector');
-
-//var path = require('path');
 var app = express();
-app.set('port', (process.env.PORT || 5000));
- /*app.get("/api",function(req,res){ 
-    connection.executeQuery('SELECT * from users',function(err,rows){
-        res.end( JSON.stringify(rows) )
-    })
- })*/
 
-app.get("/", function (req, res) {
-    res.end("hi");
-})
+app.set('port', (process.env.PORT || 5000));
+
+
+
+app.get('/', function(request, response) {
+  response.end("END");
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
