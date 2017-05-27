adfd198607039863bd2760f1b92e9e14f3968086
iankit3 <a4ankit@outlook.com>
2017-05-27 22:45:45 +0530

    added port number in the createConnection config

--- database/pool.js
+++ database/pool.js
@@ -4,6 +4,7 @@ var sqlquery = "SELECT emp_ID as E from user";
 var pool = mysql.createPool({
     connectionLimit: 10,
     host: 'ec2-54-225-107-107.compute-1.amazonaws.com',
+    port: '5432',
     user: 'jfbhhzemjqtzjy',
     database:'dki5u7o4v56in',
     password: '420d03ef38cc3a97756c737d7839bcd1a6a26d017793bab834b79565bcb34fb3'
