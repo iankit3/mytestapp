18f1e6c11ad7cb0c3f51a4267b4817b1a7d804a9
iankit3 <a4ankit@outlook.com>
2017-05-27 22:22:43 +0530

    added heroku postgres database

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
-var connector = require("./database/connector");
+//var connector = require("./database/connector.js");
+
+var connector = require("./connector.js");
 
 
 app.set('port', (process.env.PORT || 5000));
 
 
 app.get("/api",function(req,res){ 
-   connector.excuteQuery("SELECT * from users",function(err,rows){
-      var x =  JSON.stringify(rows) ;
-         res.end(x);
-
+   connector.executeQuery("SELECT * from users",function(err,rows){
+          res.end( JSON.stringify(rows) ) ;
    })
 })
 
---
+++ pool.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+var sqlquery = "SELECT emp_ID as E from user";
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'ec2-54-225-107-107.compute-1.amazonaws.com',
+    user: 'jfbhhzemjqtzjy',
+    database:'dki5u7o4v56in',
+    password: '420d03ef38cc3a97756c737d7839bcd1a6a26d017793bab834b79565bcb34fb3'
+})
+
+module.exports = pool;
