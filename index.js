e22b745b33f795d3b2caf7dd5028b0619288c73d
iankit3 <a4ankit@outlook.com>
2017-06-09 14:03:52 +0530

    twilio

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var connector = require("./database/connector.js");
+var twilio = require('twilio');
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,6 +11,19 @@ app.get("/api",function(req,res){
    connector.executeQuery("SELECT * from users",function(err,rows){
           res.end( JSON.stringify(rows) ) ;
    })
+})
+
+app.post("/welcome/sms/reply/",function(req,res){
+
+  var twilio = require('twilio');
+      var twiml = new twilio.TwimlResponse();
+          twiml.message(function() {
+		        this.body('The Robots are coming! Head for the hills!');
+			      this.media('https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg');
+			          });
+	      res.writeHead(200, {'Content-Type': 'text/xml'});
+	          res.end(twiml.toString());
+
 })
 
 app.get('/', function(request, response) {
