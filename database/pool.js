08d515b465d204e17b0943ebea82cbc45630b0f9
iankit3 <a4ankit@outlook.com>
2017-06-15 16:10:35 +0530

    port

--- database/pool.js
+++ database/pool.js
@@ -15,3 +15,15 @@ module.exports = pool;
 // psql -h ec2-54-225-107-107.compute-1.amazonaws.com -U jfbhhzemjqtzjy dki5u7o4v56in
 //-----------------to get a psql terminal------------------------------
 //password - 420d03ef38cc3a97756c737d7839bcd1a6a26d017793bab834b79565bcb34fb3
+
+/*
+ CREATE TABLE users (
+    userid   varchar(30)  primary key,
+    username varchar(200) NOT NULL,
+    address  varchar(300) NOT NULL,
+    gender   varchar(30)  default NULL
+);
+*/
+
+/*CREATE TABLE users (userid   varchar(30)  primary key,username varchar(200) NOT NULL,address  varchar(300) NOT NULL,gender   varchar(30)  default NULL);
+*/
